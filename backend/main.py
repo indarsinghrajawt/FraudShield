76dcf5025cfa5491f8ad10dc2e1f8342fb76aa93
prathvisinghrajawat555-gif <prathvisinghrajawat555@gmail.com>
2026-08-15 21:08:11 +0530

    Allow Vercel beta domain

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3001",
-        "https://fraudshield-cniec0bqb-indarsinghrajawts-projects.vercel.app"
+        "https://fraudshield-cniec0bqb-indarsinghrajawts-projects.vercel.app",
+        "https://fraudshield-beta.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
